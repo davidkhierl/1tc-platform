668ef908086ac107e55d5eb596a0c41b3fbbf85d
davidkhierl <dkrgidor@gmail.com>
2025-06-21 18:48:46 +0400

    feat: add USER command for authentication handling in CommandRegistry

---
+++ packages/supabase-ftp/src/commands/registration/user.ts
@@ -0,0 +1,35 @@
+import { CommandRegistry } from "../registry.js";
+
+const USER: CommandRegistry = {
+  directive: "USER",
+  handler: async function ({ command }) {
+    if (this.username) return this.reply(530, "Username already set");
+    if (this.authenticated) return this.reply(230);
+
+    this.username = command.arg;
+    if (!this.username) return this.reply(501, "Must provide username");
+
+    if (
+      (this.server.options.anonymous === true &&
+        this.username === "anonymous") ||
+      !!this.username === this.server.options.anonymous
+    ) {
+      return this.login(this.username, "@anonymous")
+        .then(() => {
+          return this.reply(230);
+        })
+        .catch((err) => {
+          console.error(err);
+          return this.reply(530, err.message || "Authentication failed");
+        });
+    }
+    return this.reply(331);
+  },
+  syntax: "{{cmd}} <username>",
+  description: "Authentication username",
+  flags: {
+    no_auth: true,
+  },
+};
+
+export default USER;
