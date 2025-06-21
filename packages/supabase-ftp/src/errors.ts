94b2f60ec44f3d2f0d16bda7e157a919a8221046
davidkhierl <dkrgidor@gmail.com>
2025-06-21 18:47:11 +0400

    fix: add GeneralError and SocketError classes to improve error handling

--- packages/supabase-ftp/src/errors.ts
+++ packages/supabase-ftp/src/errors.ts
@@ -1,8 +1,26 @@
+export class GeneralError extends Error {
+  readonly code: number;
+  constructor(message: string, code = 400) {
+    super(message);
+    this.code = code;
+    this.name = "GeneralError";
+  }
+}
+
 export class ConnectorError extends Error {
   readonly code: number;
   constructor(message: string, code: number = 400) {
     super(message);
     this.code = code;
     this.name = "ConnectorError";
+  }
+}
+
+export class SocketError extends Error {
+  readonly code: number;
+  constructor(message: string, code = 500) {
+    super(message);
+    this.code = code;
+    this.name = "SocketError";
   }
 }
