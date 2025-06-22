8fdf42c650ae20143a80938173fd3f39c8e8bace
davidkhierl <dkrgidor@gmail.com>
2025-06-22 14:00:50 +0400

    feat: implement TYPE command to set transfer mode (binary/ascii)

---
+++ packages/supabase-ftp/src/commands/registration/type.ts
@@ -0,0 +1,25 @@
+import { CommandRegistry } from "../registry.js";
+
+const type: CommandRegistry = {
+  directive: "TYPE",
+  handler: function ({ command }) {
+    if (!command.arg) {
+      return this.reply(501, "Must provide transfer type");
+    }
+    if (/^A[0-9]?$/i.test(command.arg)) {
+      this.transferType = "ascii";
+    } else if (/^L[0-9]?$/i.test(command.arg) || /^I$/i.test(command.arg)) {
+      this.transferType = "binary";
+    } else {
+      return this.reply(501);
+    }
+    return this.reply(200, `Switch to "${this.transferType}" transfer mode.`);
+  },
+  syntax: "{{cmd}} <mode>",
+  description: "Set the transfer mode, binary (I) or ascii (A)",
+  flags: {
+    feat: "TYPE A,I,L",
+  },
+};
+
+export default type;
