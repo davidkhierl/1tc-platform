cfa39b68e29af035b089d85cc1bd9d721bb687c4
davidkhierl <dkrgidor@gmail.com>
2025-06-22 21:56:42 +0400

    refactor: update error classes to extend SupabaseFTPError for better error handling

--- packages/supabase-ftp/src/errors.ts
+++ packages/supabase-ftp/src/errors.ts
@@ -1,26 +1,36 @@
-export class GeneralError extends Error {
+export class SupabaseFTPError extends Error {
   readonly code: number;
   constructor(message: string, code = 400) {
     super(message);
     this.code = code;
+    this.name = "SupabaseFTPError";
+  }
+}
+
+export class GeneralError extends SupabaseFTPError {
+  constructor(message: string, code = 400) {
+    super(message, code);
     this.name = "GeneralError";
   }
 }
 
-export class ConnectorError extends Error {
-  readonly code: number;
+export class ConnectorError extends SupabaseFTPError {
   constructor(message: string, code: number = 400) {
-    super(message);
-    this.code = code;
+    super(message, code);
     this.name = "ConnectorError";
   }
 }
 
-export class SocketError extends Error {
-  readonly code: number;
+export class SocketError extends SupabaseFTPError {
   constructor(message: string, code = 500) {
-    super(message);
-    this.code = code;
+    super(message, code);
     this.name = "SocketError";
+  }
+}
+
+export class FileSystemError extends SupabaseFTPError {
+  constructor(message: string, code = 400) {
+    super(message, code);
+    this.name = "FileSystemError";
   }
 }
