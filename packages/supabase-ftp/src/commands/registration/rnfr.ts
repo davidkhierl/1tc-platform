eda7a8001af6c57baba14c0613b96b285329dbbe
davidkhierl <dkrgidor@gmail.com>
2025-07-26 02:02:37 +0400

    feat(rnfr): implement RNFR command for renaming files
    - Add RNFR command to handle file renaming requests.
    - Implement error handling for file system checks and missing file names.

---
+++ packages/supabase-ftp/src/commands/registration/rnfr.ts
@@ -0,0 +1,27 @@
+import { CommandRegistry } from '../registry.js';
+
+const rnfr: CommandRegistry = {
+  directive: 'RNFR',
+  handler: async function ({ command }) {
+    const fs = this.fs;
+    if (!fs) return this.reply(550, 'File system not instantiated');
+    if (!fs.get) return this.reply(402, 'Not supported by file system');
+
+    const fileName = command.arg;
+    if (!fileName) return this.reply(501, 'Must provide file name');
+    return Promise.resolve()
+      .then(() => fs.get(fileName))
+      .then(() => {
+        this.renameFrom = fileName;
+        return this.reply(350);
+      })
+      .catch(err => {
+        console.error(err);
+        return this.reply(550, err.message);
+      });
+  },
+  syntax: '{{cmd}} <name>',
+  description: 'Rename from',
+};
+
+export default rnfr;
