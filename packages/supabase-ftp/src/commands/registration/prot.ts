76e6a80f144e8202e5049a5a038240cb20e5aaf5
davidkhierl <dkrgidor@gmail.com>
2025-07-26 02:01:59 +0400

    feat(prot): implement PROT command for data channel protection level

---
+++ packages/supabase-ftp/src/commands/registration/prot.ts
@@ -0,0 +1,29 @@
+import { CommandRegistry } from '../registry.js';
+
+const prot: CommandRegistry = {
+  directive: 'PROT',
+  handler: function ({ command }) {
+    if (!this.secure) return this.reply(202, 'Not supported');
+    if (!this.bufferSize && typeof this.bufferSize !== 'number')
+      return this.reply(503);
+
+    switch ((command?.arg || '').toUpperCase()) {
+      case 'P':
+        return this.reply(200, 'OK');
+      case 'C':
+      case 'S':
+      case 'E':
+        return this.reply(536, 'Not supported');
+      default:
+        return this.reply(504);
+    }
+  },
+  syntax: '{{cmd}}',
+  description: 'Data Channel Protection Level',
+  flags: {
+    no_auth: true,
+    feat: 'PROT',
+  },
+};
+
+export default prot;
